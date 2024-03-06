58497a9b4edfe05664cdd36e087d812249b46a03
longvu138 <tranlongvu138@gmail.com>
2024-03-06 15:04:28 +0700

    push

--- bot/bot.js
+++ bot/bot.js
@@ -2,10 +2,13 @@ import { Telegraf } from "telegraf";
 
 const token = "6433254177:AAHbH9joDYkmGvMuF0hUwsemQF8BJsWIgoo";
 const bot = new Telegraf(token);
-const web_link = "https://glowing-gumption-01f3ac.netlify.app/";
+const web_link = "https://teleapp-ochre.vercel.app/";
 
 bot.start((ctx) => {
-  console.log(ctx.from);
+  console.log("ctx", ctx);
+  console.log("ctx.from", ctx.from);
+  console.log("ctx.chat", ctx.chat);
+  console.log("ctx.entities", ctx.entities);
   // Lấy username và ID người dùng
   const username = ctx.from.username;
   const id = ctx.from.id;
